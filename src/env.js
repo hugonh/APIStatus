dc9229baa05f5f06831cb79bc8346664aea7de30
hugonh <hugobiller@hotmail.com>
2021-03-02 10:25:48 -0300

    Incident Chart

---
+++ src/env.js
@@ -0,0 +1,11 @@
+export const config = {
+    ENDPOINT: "https://v1.nocodeapi.com/hugonh/uptime/XPBjzmgSBPYqMCNj?monitors=785820041-785820037-787326309-787325770",
+
+    title: "NoCodeAPI",
+
+    website: "https://nocodeapi.com",
+
+    twitterHandle: "@nocodeapi",
+    
+    twitterURL: "https://twitter.com/nocodeapi"
+}
